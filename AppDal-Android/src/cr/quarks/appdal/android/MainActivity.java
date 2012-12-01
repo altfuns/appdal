ea7c0b3a69f01233590b942c0e3d146f20449041
Luis Alfonso Aguilar <laltfuns@gmail.com>
2012-12-01 16:06:06 -0600

    Iniciando pantalla principal y comunicación con el rest service

--- AppDal-Android/src/cr/quarks/appdal/android/MainActivity.java
+++ AppDal-Android/src/cr/quarks/appdal/android/MainActivity.java
@@ -1,20 +1,66 @@
 package cr.quarks.appdal.android;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.List;
+
+import org.restlet.data.MediaType;
+import org.restlet.resource.ClientResource;
+import org.restlet.resource.ResourceException;
+
+import cr.quarks.appdal.android.entity.Community;
+import cr.quarks.appdal.android.resource.CommunityResource;
+import cr.quarks.appdal.android.service.AppDalService;
+import cr.quarks.appdal.android.service.AppDalServiceImpl;
+import cr.quarks.appdal.android.ui.CommunityAdapter;
+import cr.quarks.appdal.android.util.BackgroundTask;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	private ListView communitiesListView;
+	
+	private List<Community> communities;
+	
+	private CommunityAdapter adapter;
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		loadStartData();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+
+	private void initComponents() {
+
+	}
+
+	private void loadStartData() {
+		new BackgroundTask() {
+
+			@Override
+			public void work() {
+				AppDalService service = new AppDalServiceImpl();
+				
+				communities = service.retrieveCommunities();
+			}
+
+			@Override
+			public void done() {
+				adapter = new CommunityAdapter();
+				adapter.setItems(communities);
+				communitiesListView.setAdapter(adapter);
+			}
+		};
+	}
 }
---
+++ AppDal-Android/src/cr/quarks/appdal/android/ui/CommunityAdapter.java
@@ -0,0 +1,84 @@
+package cr.quarks.appdal.android.ui;
+
+import java.util.List;
+
+import cr.quarks.appdal.android.R;
+import cr.quarks.appdal.android.entity.Community;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class CommunityAdapter extends BaseAdapter{
+
+	List<Community> communities;
+	
+	@Override
+	public int getCount() {
+		return communities.size();
+	}
+
+	@Override
+	public Object getItem(int position) {
+		return communities.get(position);
+	}
+
+	@Override
+	public long getItemId(int position) {
+		return communities.get(position).getId();
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		
+		CommunityViewHolder holder = null;
+        LayoutInflater inflater = (LayoutInflater) parent.getContext()
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        Community community = communities.get(position);
+        if (convertView == null) {
+            convertView = inflater.inflate(R.layout.activity_main, null);
+
+            holder = new CommunityViewHolder();
+            holder.position = (TextView) convertView
+                    .findViewById(R.id.menu_settings);
+            holder.name = (TextView) convertView
+                    .findViewById(R.id.menu_settings);
+            holder.location = (TextView) convertView
+                    .findViewById(R.id.menu_settings);
+            holder.add = (ImageView) convertView
+                    .findViewById(R.id.menu_settings);
+
+            convertView.setTag(holder);
+        } else {
+            holder = (CommunityViewHolder) convertView.getTag();
+        }
+
+        if(community != null){
+        	holder.position.setText(String.valueOf(position + 1));
+        	holder.name.setText(community.getName());
+        	holder.location.setText(community.getLocation());
+        }
+
+        return convertView;
+	}
+	
+	public void setItems(List<Community> items){
+		this.communities = items;
+		this.notifyDataSetChanged();
+	}
+
+	
+	private class CommunityViewHolder {
+        TextView position;
+
+        TextView name;
+        
+        TextView location;
+
+        ImageView add;
+    }
+}
